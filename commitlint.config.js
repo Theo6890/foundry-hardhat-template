bad562859bbd8c22b19c991f9f6ae48b5f576352
Théo RICHARD <mail@rcode.tech>
2024-06-05 16:35:20 +0200

    chore: update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,38 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+    // extends: ['@commitlint/config-conventional'],
+
+    // this overrides `extends`
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'build',
+                'chore',
+                'ci',
+                'docs',
+                'feat',
+                'fix',
+                'perf',
+                'refactor',
+                'revert',
+                'style',
+                'test',
+                'release',
+            ],
+        ],
+    },
+    /*
+     * Array of functions that return true if commitlint should ignore the given message.
+     * Given array is merged with predefined functions, which consist of matchers like:
+     *
+     * - 'Merge pull request', 'Merge X into Y' or 'Merge branch X'
+     * - 'Revert X'
+     * - 'v1.2.3' (ie semver matcher)
+     * - 'Automatic merge X' or 'Auto-merged X into Y'
+     *
+     * To see full list, check https://github.com/conventional-changelog/commitlint/blob/master/%40commitlint/is-ignored/src/defaults.ts.
+     * To disable those ignores and run rules always, set `defaultIgnores: false` as shown below.
+     */
+    ignores: [(commit) => commit === ''],
+};
