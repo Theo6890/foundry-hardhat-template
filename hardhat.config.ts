c3e8d523f907b6cb9c2f379bb2af1d2e8198fb1d
theo6890 <mail@rcode.tech>
2026-02-23 21:24:10 +0100

    chore: bump to solidity 0.8.26

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -133,7 +133,7 @@ const config: HardhatUserConfig = {
         },
     },
     solidity: {
-        version: '0.8.23',
+        version: '0.8.26',
         settings: {
             optimizer: {
                 enabled: true,
